297389180bd617a857860602295b99cfe8ad0cb9
Justin Koh <justinkoh@google.com>
2016-05-15 15:08:24 -0700

    Add value window and states. Add call to Serial.begin

--- 2-o/inflatable_o.ino/inflatable_o.ino.ino
+++ 2-o/inflatable_o.ino/inflatable_o.ino.ino
@@ -19,12 +19,25 @@
 
 const int WINDOW_SIZE = 10;
 
+const int MIN_WINDOW_ADD_DELAY_MS = 200;
+const int DEFLATE_TIME_MS = 1000;
+
 const int STATE_IDLE = 0;
 const int STATE_INFLATING = 1;
 const int STATE_DEFLATING = 2;
 
-int[] values = new int[WINDOW_SIZE];
+const int THRESHOLD_LOW = -2;
+const int THRESHOLD_HIGH = 2;
+
+int values[WINDOW_SIZE];
 int value_index = 0;
+
+unsigned long last_value_at = 0;
+unsigned long last_state_time = 0;
+
+// TODO: check and reject long lines.
+char line[10];
+char *line_pos;
 
 int state = STATE_IDLE;
 
@@ -47,25 +60,70 @@ char *getLine() {
 }
 
 void setup() {
+  Serial.begin(115200);
+  for (int i = 0; i < WINDOW_SIZE; i++) {
+    values[i] = THRESHOLD_LOW;
+  }
 }
 
 void loop() {
+
   char *line = getLine();
   if (line != NULL) {
     int num = atoi(line);
-    if (num < 0) num = 0;
-    if (num > 100) num = 100;
+
+    if (millis() - last_value_at > MIN_WINDOW_ADD_DELAY_MS) {
+      // Add window values and process them.  
+      values[value_index++] = num;
+      if (value_index >= WINDOW_SIZE) {
+        value_index = 0;
+      }
+      last_value_at = millis();
+    }
+
+    if (shouldInflate()) {
+      setState(STATE_INFLATING);
+    } else if (STATE == STATE_INFLATING) {
+      setState(STATE_DEFLATING);
+    }
   }
 
-  // Add window values and process them.
-  
   // Change state if necessary.
-   
+  if (state == STATE_DEFLATING) {
+    if (millis() - last_state_time > DEFLATE_TIME_MS) {
+      setState(STATE_IDLE);
+    }
+  } 
 }
 
-void setState(newState) {
+bool shouldInflate() {
+  int countMid = 0;
+  for (int i = 0; i < WINDOW_SIZE; i++) {
+    if (values[i] > THRESHOLD_LOW && values[i] < THRESHOLD_HIGH) {
+      countMid++;
+    }
+  }
+  return countMid > (WINDOW_SIZE - 2);
+}
+
+void setState(int newState) {
   if (state != newState) {
     state = newState;
+    last_state_time = millis();
+
+    Serial.print("new state: ");
+    Serial.println(state);
+
+    if (state == STATE_INFLATING) {
+      // Start input motor
+      
+    } else if (state == STATE_DEFLATING) {
+      // Start deflate motor
+      
+    } else if (state == STATE_IDLE) {
+      // Go idle
+      
+    }
   }
 }
 
