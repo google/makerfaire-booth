631b1e8b1865a3f9e517b8bd0ea0928b45a15b61
David Konerding <dek@google.com>
2016-05-24 13:09:29 -0700

    final makerfaire 1-o controller.

--- 1-o/mini-magneto/controller.py
+++ 1-o/mini-magneto/controller.py
@@ -18,12 +18,29 @@ def derivative(value, prev):
   return deriv
 
 
-# s = serial.Serial("/dev/ttyACM1", 115200)
+s = serial.Serial("/dev/ttyACM0", 9600)
+s.timeout = 1
+
+ready = True
+
+def read_from_port():
+  global ready
+  print("Starting to read")
+  while True:
+    reading = s.readline()
+    if "woot." == reading:
+      print("ready!")
+      ready = True 
+    #print(reading)
+
+
 def client():
   prev = None
   client_socket = gevent.socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   client_socket.connect(("127.0.0.1", 6000))
   t0 = time.time()
+  num_zeroes = 0
+  zero_threshold = 20
   while True:
     data = client_socket.recv(1024)
     try:
@@ -33,11 +50,22 @@ def client():
     else:
         if prev:
           d = derivative(value, prev)
-          print prev, value, d
+          if ready:
+            if abs(d) > 5:
+              print prev, value, d
+              s.write("%d\r\n" % d)
+              s.flush()
+              num_zeroes = 0
+            else:
+              num_zeroes += 1
+              if num_zeroes >= zero_threshold:
+                print "ZERO"
+                s.write("%d\r\n" % 0)
+                s.flush()
+
         prev = value
-    #     s.write("%d\r\n" % adj)
-    #     s.flush()
 
 client = gevent.spawn(client)
+readthread = gevent.spawn(read_from_port)
 
-gevent.joinall([client])
+gevent.joinall([client, readthread])
