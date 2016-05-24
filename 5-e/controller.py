4a0df2554edf854cae91d3106a058c2a5b1c75bc
David Konerding <dek@google.com>
2016-05-24 12:55:55 -0700

    final makerfaire 5-e controller.

--- 5-e/controller.py
+++ 5-e/controller.py
@@ -7,6 +7,8 @@ import serial
 
 #r = (0,1,5,9,14,18,23,27,32,36,41,45,50,54,59,63,68,72,77,81,86,90,95,99)
 
+DECIBEL_FLOOR = 70
+
 s = serial.Serial("/dev/ttyACM0", 9600)
 def client():
   client_socket = gevent.socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,7 +23,7 @@ def client():
       except:
           print "failed to parse", data
       else:
-          adj = int((value-20)*1.75)
+          adj = int((value-DECIBEL_FLOOR)*5)
           print adj
           s.write("%d\r" % adj)
           s.flush()
