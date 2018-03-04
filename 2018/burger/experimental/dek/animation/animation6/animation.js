1079c9c15c8bf53f252eb7eda07f4992e2493ece
dek <dakoner@gmail.com>
2018-03-04 15:31:21 -0800

    Make repeating animation.

--- 2018/burger/experimental/dek/animation/animation6/animation.js
+++ 2018/burger/experimental/dek/animation/animation6/animation.js
@@ -1,3 +1,20 @@
+function create_animation0() {
+    var element = document.getElementById("wrapper");
+
+    var timings = {
+	duration: 1000,
+	iterations: 1,
+	easing: "linear",
+	direction: "normal",
+	fill: "forwards",
+    }
+    var keyframes = [
+	{ transform: 'translateX(90vh) translateY(50vh)'},
+	{ transform: 'translateX(0vh) translateY(50vh'},
+    ];
+    return new KeyframeEffect(element, keyframes, timings);
+}
+
 function create_animation1(element_name, delay, initialY, conveyorY, finalY) {
     var element = document.getElementById(element_name);
 
@@ -43,7 +60,7 @@ function create_group(animation, f) {
     return group;
 }
 
-function create_animation4() {
+function create_animation2() {
     var element = document.getElementById("wrapper");
 
     var timings = {
@@ -55,29 +72,35 @@ function create_animation4() {
     }
     var keyframes = [
 	{ transform: 'translateX(0vh) translateY(50vh)'},
-	{ transform: 'translateX(100vh) translateY(50vh)'},
+	{ transform: 'translateX(90vh) translateY(50vh'},
     ];
     return new KeyframeEffect(element, keyframes, timings);
 }
 
 $(window).ready(function() {
     var animation = [
-	["crown", 0, -40, 16, 50],
-	["lettuce", 1000, -45, 13, 47],
-	["tomato", 2000, -50, 10, 44],
-	["cheese", 3000, -55, 7, 41],
-	["patty", 4000, -60, 4, 38],
-	["heel", 5000, -65, 1, 35],
+	["crown", 0, -40, 16, 70],
+	["lettuce", 1000, -45, 13, 67],
+	["tomato", 2000, -50, 10, 64],
+	["cheese", 3000, -55, 7, 61],
+	["patty", 4000, -60, 4, 58],
+	["heel", 5000, -65, 1, 55],
     ];
+    var group0 = create_animation0();
     var group1 = create_group(animation, create_animation1);
+    var group2 = create_animation2();
     var group3 = create_group(animation, create_animation3);
-    var group4 = create_animation4();
 
     var sequence = new SequenceEffect([
+	group0,
 	group1,
-	group4,
+	group2,
 	group3,
     ]);
     var player = new Animation(sequence, document.timeline);
     player.play();
+    
+    player.onfinish = function() {
+	player.play();
+    };
 });
