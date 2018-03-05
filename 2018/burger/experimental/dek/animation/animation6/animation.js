e2e14776b9bcd5103fb981ac08c117b65354c488
dek <dakoner@gmail.com>
2018-03-04 17:25:26 -0800

    Use a single SVG template with fill color overflow.  Add commented out programmatic SVG.

--- 2018/burger/experimental/dek/animation/animation6/animation.js
+++ 2018/burger/experimental/dek/animation/animation6/animation.js
@@ -1,6 +1,4 @@
-function create_animation0() {
-    var element = document.getElementById("wrapper");
-
+function create_animation0(wrapper) {
     var timings = {
 	duration: 1000,
 	iterations: 1,
@@ -12,7 +10,7 @@ function create_animation0() {
 	{ transform: 'translateX(90vh) translateY(50vh)'},
 	{ transform: 'translateX(0vh) translateY(50vh'},
     ];
-    return new KeyframeEffect(element, keyframes, timings);
+    return new KeyframeEffect(wrapper, keyframes, timings);
 }
 
 function create_animation1(element_name, data) {
@@ -31,6 +29,21 @@ function create_animation1(element_name, data) {
 	{ transform: 'translateY(' + data.conveyorY + 'vh)', opacity: 1},
     ];
     return new KeyframeEffect(element, keyframes, timings);
+}
+
+function create_animation2(wrapper) {
+    var timings = {
+	duration: 1000,
+	iterations: 1,
+	easing: "linear",
+	direction: "normal",
+	fill: "forwards",
+    }
+    var keyframes = [
+	{ transform: 'translateX(0vh) translateY(50vh)'},
+	{ transform: 'translateX(90vh) translateY(50vh'},
+    ];
+    return new KeyframeEffect(wrapper, keyframes, timings);
 }
 
 function create_animation3(element_name, data) {
@@ -60,47 +73,51 @@ function create_group(animation, f) {
     return group;
 }
 
-function create_animation2() {
-    var element = document.getElementById("wrapper");
-
-    var timings = {
-	duration: 1000,
-	iterations: 1,
-	easing: "linear",
-	direction: "normal",
-	fill: "forwards",
-    }
-    var keyframes = [
-	{ transform: 'translateX(0vh) translateY(50vh)'},
-	{ transform: 'translateX(90vh) translateY(50vh'},
-    ];
-    return new KeyframeEffect(element, keyframes, timings);
-}
+// $(window).load(function() {
+//     // var wrapper = document.createElement("div");
+//     var wrapper = document.getElementById("wrapper");
+//     wrapper.setAttribute('id', 'wrapper');
+//     document.body.appendChild(wrapper);
+//     var elements = [ 'crown', 'lettuce', 'tomato', 'cheese', 'patty', 'heel' ]
+//     for (i = 0; i < elements.length; i++) {
+//     	var svg = document.createElementNS(document.documentElement.namespaceURI, "svg");
+//     	svg.setAttribute('id', 'layerX' + (i+1).toString());
+//     	// can we define class crown, cheese, etc, and dynamically
+//     	// create each layer from a template SVG?
+//     	svg.setAttribute('class', elements[i]);
+//     	var use = document.createElement('use');
+//     	use.setAttribute('href', 'elements.svg#' + elements[i]);
+//     	svg.appendChild(use);
+//     	wrapper.appendChild(svg);
+//     }
+// });
 
 $(window).ready(function() {
+    var wrapper = document.getElementById("wrapper");
     var animation = [
-	["crown", {delay: 0, initialY: -40, conveyorY: 16, finalY: 70}],
-	["lettuce", {delay:1000, initialY:-45, conveyorY: 13, finalY: 67}],
-	["tomato", {delay:2000, initialY:-50, conveyorY: 10, finalY: 64}],
-	["cheese", {delay:3000, initialY:-55, conveyorY: 7, finalY: 61}],
-	["patty", {delay:4000, initialY: -60, conveyorY: 4, finalY: 58}],
-	["heel", {delay:5000, initialY: -65, conveyorY: 1, finalY: 55}],
+    	["layer1", {delay: 0, initialY: -40, conveyorY: 16, finalY: 70}],
+    	["layer2", {delay:1000, initialY:-45, conveyorY: 13, finalY: 67}],
+    	["layer3", {delay:2000, initialY:-50, conveyorY: 10, finalY: 64}],
+    	["layer4", {delay:3000, initialY:-55, conveyorY: 7, finalY: 61}],
+    	["layer5", {delay:4000, initialY: -60, conveyorY: 4, finalY: 58}],
+    	["layer6", {delay:5000, initialY: -65, conveyorY: 1, finalY: 55}],
     ];
-    var group0 = create_animation0();
+    var group0 = create_animation0(wrapper);
     var group1 = create_group(animation, create_animation1);
-    var group2 = create_animation2();
+    var group2 = create_animation2(wrapper);
     var group3 = create_group(animation, create_animation3);
 
     var sequence = new SequenceEffect([
-	group0,
-	group1,
-	group2,
-	group3,
+    	group0,
+    	group1,
+    	group2,
+    	group3,
     ]);
     var player = new Animation(sequence, document.timeline);
     player.play();
     
     player.onfinish = function() {
-	player.play();
+    	player.play();
     };
 });
+
