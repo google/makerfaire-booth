81c1e602fc00a89e53bc494088dc3c83018e9075
dek <dakoner@gmail.com>
2018-03-18 12:12:01 -0700

    Make multi-position burger conveyor work.

--- 2018/burger/trainer/animation.js
+++ 2018/burger/trainer/animation.js
@@ -1,12 +1,11 @@
-
-function create_animation1(element_name, data) {
-    var element = document.getElementById(element_name);
+function create_animation1(element_name, data, wrapper) {
+    var element = wrapper.children.namedItem(element_name);
     var body = document.getElementsByTagName("BODY")[0];
-    width = body.getBoundingClientRect().width;
-    height = body.getBoundingClientRect().height;
+    var width = body.getBoundingClientRect().width;
+    var height = body.getBoundingClientRect().height;
 
     var timings = {
-	duration: 1000,
+	duration: 500,
 	iterations: 1,
 	easing: "linear",
 	direction: "normal",
@@ -21,53 +20,58 @@ function create_animation1(element_name, data) {
     return new KeyframeEffect(element, keyframes, timings);
 }
 
-function create_animation2(element_name, data) {
-    var element = document.getElementById(element_name);
+
+function create_animation2(element_name, data, wrapper) {
+    var element = wrapper.children.namedItem(element_name);
+    var dest = wrapper.getAttribute('id');
     var body = document.getElementsByTagName("BODY")[0];
-    width = body.getBoundingClientRect().width;
-    height = body.getBoundingClientRect().height;
+    var width = body.getBoundingClientRect().width;
+    var height = body.getBoundingClientRect().height;
 
     var timings = {
-	duration: 1000,
+	duration: 500,
 	iterations: 1,
 	easing: "linear",
 	direction: "normal",
 	fill: "forwards",
     }
 
-    var dest = data.dest == 'left' ? 0 : width-256;
+    var target = dest == 'left' ? 0 : width-256;
+    
     var keyframes = [
 	{ transform: 'translateX(' + (width/2 - 128) + 'px) translateY(' + data.conveyorY + ')', opacity: 1},
-	{ transform: 'translateX(' + dest + 'px) translateY(' + data.conveyorY + ')', opacity: 1},
+	{ transform: 'translateX(' + target + 'px) translateY(' + data.conveyorY + ')', opacity: 1},
     ];
     return new KeyframeEffect(element, keyframes, timings);
 }
 
-function create_animation3(element_name, data) {
-    var element = document.getElementById(element_name);
+function create_animation3(element_name, data, wrapper) {
+    var element = wrapper.children.namedItem(element_name);
+    var dest = wrapper.getAttribute('id');
     var body = document.getElementsByTagName("BODY")[0];
     width = body.getBoundingClientRect().width;
     height = body.getBoundingClientRect().height;
 
     var timings = {
-	duration: 1000,
+	duration: 500,
 	iterations: 1,
 	easing: "linear",
 	direction: "normal",
 	fill: "forwards",
     }
-    var dest = data.dest == 'left' ? 0 : width-256;
+    var target = dest == 'left' ? 0 : width-256;
     var keyframes = [
-	{ transform: 'translateX(' + dest + 'px) translateY(' + data.conveyorY + ')', opacity: 1},
-	{ transform: 'translateX(' + dest + 'px) translateY(' + data.finalY + ')', opacity: 1},
+	{ transform: 'translateX(' + target + 'px) translateY(' + data.conveyorY + ')', opacity: 1},
+	{ transform: 'translateX(' + target + 'px) translateY(' + data.finalY + ')', opacity: 1},
     ];
     return new KeyframeEffect(element, keyframes, timings);
 }
 
-function create_group(animation, f) {
+function create_group(wrapper, f) {
+    var animation = create_animation();
     var kEffects = [];
     for (let a of animation) {
-	var kf = f(a[0], a[1]);
+	var kf = f(a[0], a[1], wrapper);
 	kEffects.push(kf);
     }
     var group = new GroupEffect(kEffects);
@@ -77,7 +81,8 @@ function create_group(animation, f) {
 function create_burger(layers) {
     var svgNS = 'http://www.w3.org/2000/svg';
     var wrapper = document.createElement("div");
-    wrapper.setAttribute('id', "wrapper");
+    wrapper.setAttribute('class', "wrapper");
+    wrapper.setAttribute('id', 'hopper');
     for (i = 0; i < layers.length; i++) {
     	var svg = document.createElementNS(svgNS, "svg");
     	svg.setAttributeNS(null, 'id', 'layer' + (i+1).toString());
@@ -116,19 +121,49 @@ function create_chute() {
     chuteline2.setAttributeNS(null, 'y2', height);
 }
 
-var leftBurger = null;
-var rightBurger = null;
-
-function start_animation() {
-    var layers = [ 'crown', 'crown', 'crown', 'lettuce', 'tomato', 'cheese', 'patty', 'heel' ];
-    var wrapper = create_burger(layers);
-    document.body.appendChild(wrapper);
+function create_animation() {
     var baseInitialY = -300;
     var baseConveyorY = 420;
     var baseFinalY = 1000;
     var animation = [];
-    var dest;
+    for (i = 0; i < 8; i++) {
+	animation.push(["layer" + (7-i+1), {
+	    delay: i*100,
+	    initialY: (baseInitialY - i * 50) + 'px',
+	    conveyorY: (baseConveyorY - i * 50) + 'px',
+	    finalY: (baseFinalY - i * 50) + 'px',
+	}]);
+    }
+    return animation;
+}
+
+function start_animation() {
+    console.log("starting a new animation if needed");
+    var hopperBurger = document.getElementById('hopper');
+    if (hopperBurger != null) {	
+	console.log("hopper is not empty, conveying burger.");
+	convey_burger(hopperBurger);
+	return;
+    }
+    console.log("Animationg a new burger in the hopper");
+    var layers = [ 'crown', 'crown', 'crown', 'lettuce', 'tomato', 'cheese', 'patty', 'heel' ];
+    hopperBurger = create_burger(layers);
+    document.body.appendChild(hopperBurger);
+    var group1 = create_group(hopperBurger, create_animation1);
+
+    var player = new Animation(group1, document.timeline);
+    player.onfinish = function() {
+	convey_burger(hopperBurger);
+    }
+    player.play();
+}
+
+function convey_burger(wrapper) {
+    console.log("conveying burger if needed.");
     // use leftBurger and rightBurger to determine if we need to make a random choice;
+    var dest;
+    var leftBurger = document.getElementById('left');
+    var rightBurger = document.getElementById('right');
     if (leftBurger == null && rightBurger == null) {
 	if (Math.random() < 0.5) {
 	    dest = 'left';
@@ -140,72 +175,71 @@ function start_animation() {
     } else if (rightBurger == null) {
 	dest = 'right';
     } else {
-	console.log("Can't start animation yet.");
+	console.log("Cannot convey burger.");
 	return;
     }
-    for (i = 0; i < 8; i++) {
-	animation.push(["layer" + (7-i+1), {
-	    delay: i*1000,
-	    initialY: (baseInitialY - i * 50) + 'px',
-	    conveyorY: (baseConveyorY - i * 50) + 'px',
-	    finalY: (baseFinalY - i * 50) + 'px',
-	    dest: dest
-	}]);
-    }
-    var group1 = create_group(animation, create_animation1);
-    var group2 = create_group(animation, create_animation2);
+    console.log("Chose conveyor dest:", dest);
+    wrapper.setAttribute('id', dest);
 
-    var sequence = new SequenceEffect([
-    	group1,
-    	group2,
-    ]);
-    
-    var player = new Animation(sequence, document.timeline);
-    player.onfinish = wait_for_keypress(animation, dest, wrapper);
+    var group2 = create_group(wrapper, create_animation2);
+    var player = new Animation(group2, document.timeline);
+    player.onfinish = function() {
+	wait_for_keypress(wrapper);
+    }
     player.play();
 }
 
-function after_keypress(animation, keydownHandler, dest, wrapper) {
-    var group = create_group(animation, create_animation3);
+function after_keypress(keydownHandler, wrapper) {
+    console.log("after keypress for", wrapper);
+    var dest = wrapper.getAttribute('id');
+    console.log("dest is", dest);
+    var animation = create_animation();
+    var group = create_group(wrapper, create_animation3);
     var player = new Animation(group, document.timeline);
-    document.removeEventListener('keydown', keydownHandler);
     player.onfinish = function() {
 	if (dest == 'left') {
-	    leftBurger = null;
+	    leftBurger = false;
 	} else if (dest == 'right') {
-	    rightBurger = null;
+	    rightBurger = false;
 	}
 	wrapper.remove();
+	start_animation();
     }
     player.play();
 }
 
-function wait_for_keypress(animation, dest, wrapper) {
-    start_animation();
+function wait_for_keypress(wrapper) {
+    console.log("wait for keypress for", wrapper);
+    var dest = wrapper.getAttribute('id');
+    console.log("dest is", dest);
     var yesCode, noCode;
-    if (dest == 'left') {
-	leftBurger = wrapper;
-	yesCode = "Z".charCodeAt(0);
-	noCode = "X".charCodeAt(0);
-    } else if (dest == 'right') {
-	rightBurger = wrapper;
-	yesCode = "N".charCodeAt(0);
-	noCode = "M".charCodeAt(0);
-    } else {
-	console.log("Unexpected dest:" + dest);
-	return;
-    }
-    function keydownHandler(e) {
+    function keydownHandlerLeft(e) {
+	var yesCode = "Z".charCodeAt(0);
+	var noCode = "X".charCodeAt(0);
+	console.log("keydownHandlerLeft waiting for", yesCode, noCode, "got", e.keyCode);
 	if (e.keyCode == yesCode || e.keyCode == noCode) {
-	    after_keypress(animation, this.keydownHandler, dest, wrapper);
+	    document.removeEventListener('keydown', this);
+	    after_keypress(this.keydownHandler, wrapper);
 	}
     }
-    document.addEventListener('keydown', keydownHandler, false);
+    function keydownHandlerRight(e) {
+	var yesCode = "M".charCodeAt(0);
+	var noCode = "N".charCodeAt(0);
+	console.log("keydownHandlerRight waiting for", yesCode, noCode, "got", e.keyCode);
+	if (e.keyCode == yesCode || e.keyCode == noCode) {
+	    document.removeEventListener('keydown', this);
+	    after_keypress(this.keydownHandler, wrapper);
+	}
+    }
+    if (dest == 'left')
+	document.addEventListener('keydown', keydownHandlerLeft);
+    else if (dest == 'right')
+	document.addEventListener('keydown', keydownHandlerRight);
+    start_animation();
 }
 
 $(window).ready(function() {
     create_chute();
-    var layers = [ 'crown', 'crown', 'crown', 'lettuce', 'tomato', 'cheese', 'patty', 'heel' ];
     start_animation();
 });
 
