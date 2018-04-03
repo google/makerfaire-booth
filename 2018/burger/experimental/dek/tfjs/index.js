cd5ca4b978a3cfa52a415bbd0dcacf1bd5ee30c7
dek <dakoner@gmail.com>
2018-04-03 08:57:48 -0700

    starter for tf.js.  classify burger w/ mobilenet.

---
+++ 2018/burger/experimental/dek/tfjs/index.js
@@ -0,0 +1,36 @@
+import * as tfc from '@tensorflow/tfjs-core';
+import {MobileNet} from './mobilenet';
+// import imageURL from './cat.jpg';
+import imageURL from './burger_000156.png';
+
+const cat = document.getElementById('cat');
+cat.onload = async () => {
+    const resultElement = document.getElementById('result');
+
+    resultElement.innerText = 'Loading MobileNet...';
+
+    const mobileNet = new MobileNet();
+    console.time('Loading of model');
+    await mobileNet.load();
+    console.timeEnd('Loading of model');
+
+    const pixels = tfc.fromPixels(cat);
+
+    console.time('First prediction');
+    let result = mobileNet.predict(pixels);
+    const topK = mobileNet.getTopKClasses(result, 5);
+    console.timeEnd('First prediction');
+
+    resultElement.innerText = '';
+    topK.forEach(x => {
+	resultElement.innerText += `${x.value.toFixed(3)}: ${x.label}\n`;
+    });
+
+    console.time('Subsequent predictions');
+    result = mobileNet.predict(pixels);
+    mobileNet.getTopKClasses(result, 5);
+    console.timeEnd('Subsequent predictions');
+
+    mobileNet.dispose();
+};
+cat.src = imageURL;
