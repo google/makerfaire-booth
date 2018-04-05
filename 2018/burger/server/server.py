a6287bf641d17595cf1ba10b9a6ecbbefd68065c
dek <dakoner@gmail.com>
2018-04-05 10:13:30 -0700

    add CORS headers

--- 2018/burger/server/server.py
+++ 2018/burger/server/server.py
@@ -21,6 +21,11 @@ class IndexHandler(tornado.web.RequestHandler):
         self.write('ok')
 
 class VoteHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+                                            
     def get(self):
         burger = self.get_argument('burger', '000000')
         vote = bool(self.get_argument('vote', 'True'))
