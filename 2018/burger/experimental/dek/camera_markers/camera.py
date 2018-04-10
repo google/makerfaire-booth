a0715c87e75bd96c0432ec8915774f80098fef6a
dek <dakoner@gmail.com>
2018-04-10 05:57:25 -0700

    show homography-corrected image.

--- 2018/burger/experimental/dek/camera_markers/camera.py
+++ 2018/burger/experimental/dek/camera_markers/camera.py
@@ -16,12 +16,18 @@ class MainWindow(QtGui.QMainWindow):
         self.setCentralWidget(self.central_widget)
         self.image_widget = QtGui.QLabel(self)
         self.central_layout.addWidget(self.image_widget)
-
+        self.image2_widget = QtGui.QLabel(self)
+        self.central_layout.addWidget(self.image2_widget)
+        
         self.camera()
         
-    def imageTo(self, image):
+    def imageTo(self, image, image2):
         pixmap = QtGui.QPixmap.fromImage(image)
-        self.image_widget.setPixmap(pixmap);
+        p = pixmap.scaledToWidth(1024)
+        self.image_widget.setPixmap(p)
+        if image2:
+            pixmap2 = QtGui.QPixmap.fromImage(image2)
+            self.image2_widget.setPixmap(pixmap2)
     
     def camera(self):
         self.camera = CameraReader()
@@ -29,7 +35,7 @@ class MainWindow(QtGui.QMainWindow):
         self.camera.signal.connect(self.imageTo)
 
 class CameraReader(QtCore.QThread):
-    signal = QtCore.Signal(QtGui.QImage)
+    signal = QtCore.Signal(QtGui.QImage, QtGui.QImage)
     def __init__(self):
         super(CameraReader, self).__init__()
         self.cam = cv2.VideoCapture(0)
@@ -54,15 +60,54 @@ class CameraReader(QtCore.QThread):
                 h, w, _ = img.shape
 
                 corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(img, self.aruco_dict, parameters=self.parameters)
-                cv2.aruco.drawDetectedMarkers(img,corners, ids)
+                cv2.aruco.drawDetectedMarkers(img, corners, ids)
                 for corner in corners:
-                    rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corner, 0.05, newcameramtx, self.dist)
+                    rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corner, 0.195, newcameramtx, self.dist)
                     try:
                         cv2.aruco.drawAxis(img, newcameramtx, self.dist, rvec, tvec, 0.1)
                     except cv2.error:
                         print "bad matrix"
-                image = QtGui.QImage(img.data, w, h, QtGui.QImage.Format_RGB888).rgbSwapped()
-                self.signal.emit(image)
+
+                d = {}
+                short_ids = [id_[0] for id_ in ids]
+                for i, corner in enumerate(corners):
+                    d[short_ids[i]] = corner
+                try:
+                    ul = d[3][0][2]
+                    ur = d[2][0][3]
+                    lr = d[0][0][0]
+                    ll = d[1][0][1]
+                except:
+                    image = QtGui.QImage(img.data, w, h, QtGui.QImage.Format_RGB888).rgbSwapped()
+                    image2 = None
+                else:
+                    pts = np.array([ul, ur, lr, ll], np.int32)
+                    pts = pts.reshape((-1,1,2))
+                    cv2.polylines(img,[pts],True,(0,255,255))
+                    # rect = cv2.boundingRect(np.array([ul, ur, lr, ll]))
+                    # ul = rect[0], rect[1]
+                    # ur = rect[0] + rect[2], rect[1]
+                    # lr = rect[0] + rect[2], rect[1] + rect[3]
+                    # ll = rect[0], rect[1] + rect[3]
+                    # pts = np.array([ul, ur, lr, ll], np.int32)
+                    # pts = pts.reshape((-1,1,2))
+                    # cv2.polylines(img,[pts],True,(255,0,255))
+
+                    # crop = img[rect[1]:rect[1]+rect[3],
+                    #            rect[0]:rect[0]+rect[2]]
+
+                    # crop = np.ascontiguousarray(crop)
+                    # crop_h, crop_w, _ = crop.shape
+                    image = QtGui.QImage(img.data, w, h, QtGui.QImage.Format_RGB888).rgbSwapped()
+                    # image2 = QtGui.QImage(crop.data, crop_w, crop_h, 3*crop_w, QtGui.QImage.Format_RGB888).rgbSwapped()
+
+                    destCorners = np.array([ [0, 0], [128, 0], [128, 128], [0, 128]], dtype=np.float32)
+                    srcCorners = np.array([ul, ur, lr, ll], dtype=np.float32)
+                    pt = cv2.getPerspectiveTransform(srcCorners, destCorners)
+                    result = cv2.warpPerspective(img, pt, (128, 128))
+                    image2 = QtGui.QImage(result.data, 128, 128, 3*128, QtGui.QImage.Format_RGB888).rgbSwapped()
+
+                self.signal.emit(image, image2)
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal.SIG_DFL)
