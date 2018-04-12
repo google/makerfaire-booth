1cacc330ea842392f349f5ecefd37d19f5a3d304
David Konerding <dakoner@gmail.com>
2018-04-12 14:58:19 -0700

    calibration script improvement.

--- 2018/burger/experimental/dek/camera_markers/camera_calibrate.py
+++ 2018/burger/experimental/dek/camera_markers/camera_calibrate.py
@@ -20,36 +20,50 @@ class MainWindow(QtGui.QMainWindow):
         self.camera.start()
         self.button = QtGui.QPushButton(self, "Hello")
         self.button.clicked.connect(self.camera.calibrate)
-        
+        self.central_layout.addWidget(self.button)
+        self.button2 = QtGui.QPushButton(self, "Consider")
+        self.button2.clicked.connect(self.camera.consider)
+        self.central_layout.addWidget(self.button2)
+
     def imageTo(self, image):
         pixmap = QtGui.QPixmap.fromImage(image)
         self.image_widget.setPixmap(pixmap);
-    
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*8,3), np.float32)
+objp[:,:2] = np.mgrid[0:8,0:6].T.reshape(-1,2)
+
 class CameraReader(QtCore.QThread):
     signal = QtCore.Signal(QtGui.QImage)
     def __init__(self):
         super(CameraReader, self).__init__()
         self.cam = cv2.VideoCapture(0)
-        self.width = long(self.cam.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.height = long(self.cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.width = int(self.cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.height = int(self.cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
         self.aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_1000)
         self.parameters =  cv2.aruco.DetectorParameters_create()
 
+
         self.objpoints = [] # 3d point in real world space
         self.imgpoints = [] # 2d points in image plane
+
+        self.last = None
         
     def calibrate(self):
         rms, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(self.objpoints, self.imgpoints, (self.width, self.height), None,None)
-        pickle.dump([rms, mtx, dist, rvecs, tvecs], open("calib.pkl", "w"))
-            
+        pickle.dump([rms, mtx, dist, rvecs, tvecs], open("calib.pkl", "wb"))
+        sys.exit(1)
+
+    def consider(self):
+        print("consider")
+        if self.last is not None:
+            self.objpoints.append(objp)
+            self.imgpoints.append(self.last)
         
     def run(self):
         # termination criteria
         criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-        # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-        objp = np.zeros((6*8,3), np.float32)
-        objp[:,:2] = np.mgrid[0:8,0:6].T.reshape(-1,2)
 
 
         while True:
@@ -58,8 +72,7 @@ class CameraReader(QtCore.QThread):
             if ret == True:
                 ret, corners = cv2.findChessboardCorners(img, (8,6),None)
                 if ret == True:
-                    self.objpoints.append(objp)
-                    self.imgpoints.append(corners)
+                    self.last = corners
                     img = cv2.drawChessboardCorners(img, (8,6), corners, ret)
                 image = QtGui.QImage(img.data, self.width, self.height, QtGui.QImage.Format_RGB888).rgbSwapped()
                 self.signal.emit(image)
