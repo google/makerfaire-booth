532224f916ded0fc3d7ae4d9ab140e45b1f1fb60
dek <dakoner@gmail.com>
2018-04-13 12:51:07 -0700

    add scaling support

--- 2018/burger/experimental/dek/basic_cv_tools/basic_cv_tools.py
+++ 2018/burger/experimental/dek/basic_cv_tools/basic_cv_tools.py
@@ -4,6 +4,30 @@ import sys
 from PySide import QtGui, QtCore, QtSvg
 import cv2
 
+class QGraphicsSvgItem(QtSvg.QGraphicsSvgItem):
+    def __init__(self, *args, **kwargs):
+        super(QGraphicsSvgItem, self).__init__(*args, **kwargs)
+
+        self.press = None
+        
+    def mouseMoveEvent(self, event):
+        if (event.buttons() & QtCore.Qt.LeftButton) and event.modifiers() & QtCore.Qt.ControlModifier:
+            delta = event.scenePos() - self.press
+            
+            scale = self.scale() + delta.x()/50
+            if scale > 0:
+                self.setScale(scale)
+            return
+        return super(QGraphicsSvgItem, self).mouseMoveEvent(event)
+
+    def mousePressEvent(self, event):
+        if (event.buttons() & QtCore.Qt.LeftButton) and event.modifiers() & QtCore.Qt.ControlModifier:
+            print "blah press", event.scenePos()
+            self.press = event.scenePos()
+            return
+        
+        return super(QGraphicsSvgItem, self).mousePressEvent(event)
+
 class Widget(QtGui.QWidget):
     def __init__(self):
         QtGui.QWidget.__init__(self)
@@ -15,8 +39,10 @@ class Widget(QtGui.QWidget):
         self.view.setFixedSize(1024,768)
         self.view.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
 	self.view.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        patty = QtSvg.QGraphicsSvgItem("../../../assets/patty.svg")
+        self.view.setMouseTracking(True)
+        patty = QGraphicsSvgItem("../../../assets/patty.svg")
         patty.setFlags(QtGui.QGraphicsItem.ItemIsMovable)
+        patty.setScale(2)
         self.scene.addItem(patty)
 
         self.image_widget = QtGui.QLabel(self)
@@ -38,14 +64,15 @@ class Widget(QtGui.QWidget):
         img = numpy.fromstring(image.constBits(), dtype=numpy.uint8).reshape(768, 1024, 3)
         cv2.imwrite("test.png", img)
         edged = cv2.Canny(img, 40, 200)
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
         closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
         im2, contours, hierarchy = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-        for c in contours:
-                peri = cv2.arcLength(c, True)
-                approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-                cv2.drawContours(img, [approx], -1, (0, 255, 0), 1)
+        if len(contours):
+            c = contours[0]
+            peri = cv2.arcLength(c, True)
+            approx = cv2.approxPolyDP(c, 0.01 * peri, True)
+            cv2.drawContours(img, [approx], -1, (0, 255, 0), 1)
 
 
         image = QtGui.QImage(img.data, 1024, 768, QtGui.QImage.Format_RGB888)
