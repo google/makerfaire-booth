09f1923c538794f659daba41ed8a6887e45caba6
dek <dakoner@gmail.com>
2018-04-13 07:10:50 -0700

    minor rename

--- 2018/burger/experimental/dek/train_object_detector/export.sh
+++ 2018/burger/experimental/dek/train_object_detector/export.sh
@@ -4,4 +4,4 @@ python $MODELS_RESEARCH_DIR/object_detection/export_inference_graph.py \
        --input_type=image_tensor \
        --pipeline_config_path=models/model/ssd_mobilenet_v1_burgers.config \
        --trained_checkpoint_prefix=train/model.ckpt-0 \
-       --output_directory=output_inference_graph.pb
+       --output_directory=output_inference_graph
