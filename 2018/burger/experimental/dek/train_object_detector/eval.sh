86d3c286936b3b17750d5668cdaeb04c12577a6f
David Konerding <dakoner@gmail.com>
2018-04-17 17:28:08 +0000

    make eval.sh use gs: path

--- 2018/burger/experimental/dek/train_object_detector/eval.sh
+++ 2018/burger/experimental/dek/train_object_detector/eval.sh
@@ -2,6 +2,5 @@ source conf.sh
 python $MODELS_RESEARCH_DIR/object_detection/eval.py \
        --logtostderr \
        --pipeline_config_path=models/model/faster_rcnn_resnet101_burgers.config \
-       --checkpoint_dir=models/train \
-       --eval_dir=models/eval
-       #--pipeline_config_path=models/model/ssd_mobilenet_v1_burgers.config \
+       --checkpoint_dir=gs://seventh-oven-198801-burgers/models/train \
+       --eval_dir=gs://seventh-oven-198801-burgers/models/eval
