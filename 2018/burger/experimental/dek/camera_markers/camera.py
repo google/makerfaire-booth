c08c68239a1fbcbc35df09e781489ae898e5e2e8
dek <dakoner@gmail.com>
2018-04-26 16:43:43 -0700

    Cleanups and improvements.

--- 2018/burger/experimental/dek/camera_markers/camera.py
+++ 2018/burger/experimental/dek/camera_markers/camera.py
@@ -61,6 +61,7 @@ class CameraReader(QtCore.QThread):
     signal2 = QtCore.pyqtSignal(QtGui.QImage,object)
     def __init__(self):
         super(CameraReader, self).__init__()
+        # self.cam = cv2.VideoCapture("/home/dek/my_video-1.mkv")
         self.cam = cv2.VideoCapture(0)
         # self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
         # self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
@@ -79,15 +80,15 @@ class CameraReader(QtCore.QThread):
                 newcameramtx, roi=cv2.getOptimalNewCameraMatrix(self.mtx, self.dist, (w,h), 1, (w,h))
                 dst = cv2.undistort(img, self.mtx, self.dist, None, newcameramtx)
                 # # crop the image
-                x, y, w, h = roi
-                dst = dst[y:y+h, x:x+w]
+                x, y, w2, h2 = roi
+                dst = dst[y:y+h2, x:x+w2]
                 img = np.ascontiguousarray(dst, dst.dtype)
-                h, w, _ = img.shape
+                h3, w3, _ = img.shape
 
                 corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(img, self.aruco_dict, parameters=self.parameters)
                 cv2.aruco.drawDetectedMarkers(img, corners, ids)
                 for corner in corners:
-                    rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corner, 0.195, newcameramtx, self.dist)
+                    rvec, tvec = cv2.aruco.estimatePoseSingleMarkers(corner, 0.195, newcameramtx, self.dist)
                     try:
                         cv2.aruco.drawAxis(img, newcameramtx, self.dist, rvec, tvec, 0.1)
                     except cv2.error:
@@ -97,58 +98,58 @@ class CameraReader(QtCore.QThread):
                 image2 = QtGui.QImage(self.width, self.height, QtGui.QImage.Format_RGB888)
                 boxes = []
                 if ids is None:
-                    image = QtGui.QImage(img.data, w, h, w*3, QtGui.QImage.Format_RGB888).rgbSwapped()
-                    print("emit image")
+                    image = QtGui.QImage(img.data, w3, h3, w3*3, QtGui.QImage.Format_RGB888).rgbSwapped()
                     self.signal.emit(image)
                 else:
                     short_ids = [id_[0] for id_ in ids]
                     for i, corner in enumerate(corners):
                         d[short_ids[i]] = corner
-                    try:
-                        ul = d[0][0][0]
-                        ur = d[1][0][0]
-                        lr = d[2][0][0]
-                        ll = d[3][0][0]
-                    except:
-                        image = QtGui.QImage(img.data, w, h, w*3, QtGui.QImage.Format_RGB888).rgbSwapped()
+                    if 0 not in d or 2 not in d or 3 not in d or 4 not in d:
                         self.signal.emit(image)
-                    else:
-                        pts = np.array([ul, ur, lr, ll], np.int32)
-                        pts = pts.reshape((-1,1,2))
-                        cv2.polylines(img,[pts],True,(0,255,255))
-                        image = QtGui.QImage(img.data, w, h, w*3, QtGui.QImage.Format_RGB888).rgbSwapped()
+                        continue
+                    ul = d[2][0][0]
+                    ur = d[0][0][1]
+                    lr = d[4][0][2]
+                    ll = d[3][0][3]
+                    pts = np.array([ul, ur, lr, ll], np.int32)
+                    pts = pts.reshape((-1,1,2))
+                    cv2.polylines(img,[pts],True,(0,255,255))
+                    image = QtGui.QImage(img.data, w3, h3, w3*3, QtGui.QImage.Format_RGB888).rgbSwapped()
+                    self.signal.emit(image)
 
-                        warped_height = 640
-                        orig_width = ur[0] - ul[0]
-                        orig_height = ll[1] - ul[1]
-                        ratio = orig_width / float(orig_height)
-                        warped_width = int(round(warped_height * ratio))
-                        destCorners = np.array([ [0, 0], [warped_width, 0], [warped_width, warped_height], [0, warped_height]], dtype=np.float32)
-                        srcCorners = np.array([ul, ur, lr, ll], dtype=np.float32)
-                        pt = cv2.getPerspectiveTransform(srcCorners, destCorners)
-                        warped = cv2.warpPerspective(img, pt, (warped_width, warped_height))
-                        warped_image = QtGui.QImage(warped.data, warped_width, warped_height, 3*warped_width, QtGui.QImage.Format_RGB888).rgbSwapped()
-                        image2 = QtGui.QImage(warped_width*2, warped_height*2, QtGui.QImage.Format_RGB888)
+                    warped_height = 480
+                    orig_width = ur[0] - ul[0]
+                    orig_height = ll[1] - ul[1]
+                    ratio = orig_width / float(orig_height)
+                    warped_width = int(round(warped_height * ratio))
+                    destCorners = np.array([ [0, 0], [warped_width, 0], [warped_width, warped_height], [0, warped_height]], dtype=np.float32)
+                    srcCorners = np.array([ul, ur, lr, ll], dtype=np.float32)
+                    pt = cv2.getPerspectiveTransform(srcCorners, destCorners)
+                    warped = cv2.warpPerspective(img, pt, (warped_width, warped_height))
+                    warped_image = QtGui.QImage(warped.data, warped_width, warped_height, 3*warped_width, QtGui.QImage.Format_RGB888).rgbSwapped()
+                    image2 = QtGui.QImage(warped_width*2, warped_height*2, QtGui.QImage.Format_RGB888)
 
-                        image2.fill(QtCore.Qt.white);
-                        painter = QtGui.QPainter(image2)
-                        point = QtCore.QPoint(warped_width/2, warped_height/2)
-                        painter.drawImage(point, warped_image)
-                        painter.end()
-                        bits = image2.constBits().asstring(warped_height*2 * warped_width*2 * 3)
-                        img = np.fromstring(bits, dtype=np.uint8).reshape(warped_height*2, warped_width*2, 3)
-                        expand = np.expand_dims(img, axis=0)
-                        result = self.objdet.detect(expand)
-                        for i in range(result['num_detections']):
-                            if result['detection_scores'][i] > 0.4:
-                                class_ = result['detection_classes'][i]
-                                box = result['detection_boxes'][i]
-                                score = result['detection_scores'][i]
-                                y1, x1 = box[0] * warped_height*2, box[1] * warped_width*2
-                                y2, x2 = box[2] * warped_height*2, box[3] * warped_width*2
-                                boxes.append((class_, score, x1, y1, x2, y2))
-                        self.signal2.emit(image2, boxes)
-
+                    image2.fill(QtCore.Qt.white);
+                    painter = QtGui.QPainter(image2)
+                    point = QtCore.QPoint(warped_width/2, warped_height/2)
+                    painter.drawImage(point, warped_image)
+                    painter.end()
+                    bits = image2.constBits().asstring(warped_height*2 * warped_width*2 * 3)
+                    img = np.fromstring(bits, dtype=np.uint8).reshape(warped_height*2, warped_width*2, 3)
+                    expand = np.expand_dims(img, axis=0)
+                    result = self.objdet.detect(expand)
+                    for i in range(result['num_detections']):
+                        if result['detection_scores'][i] > 0.1:
+                            class_ = result['detection_classes'][i]
+                            box = result['detection_boxes'][i]
+                            score = result['detection_scores'][i]
+                            y1, x1 = box[0] * warped_height*2, box[1] * warped_width*2
+                            y2, x2 = box[2] * warped_height*2, box[3] * warped_width*2
+                            boxes.append((class_, score, x1, y1, x2, y2))
+                    self.signal2.emit(image2, boxes)
+            # else:
+            #     self.cam = cv2.VideoCapture("/home/dek/my_video-1.mkv")
+                
 
 
 if __name__ == '__main__':
