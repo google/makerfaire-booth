3f80c24537957dfa73fb6ad45413b138f1488c0f
dek <dakoner@gmail.com>
2018-04-27 16:55:29 -0700

    Resize works.

--- 2018/burger/experimental/dek/image_labeller/image_labeller.py
+++ 2018/burger/experimental/dek/image_labeller/image_labeller.py
@@ -2,35 +2,71 @@ import sys
 import signal
 from PyQt5 import QtGui, QtCore, QtWidgets
 
+NO_STATE=0
+MOVE=1
+RESIZE=2
 class QGraphicsRectItem(QtWidgets.QGraphicsRectItem):
     def __init__(self, *args, **kwargs):
         super(QGraphicsRectItem, self).__init__(*args, **kwargs)
-
+        self.setFlags(QtWidgets.QGraphicsItem.ItemIsMovable | QtWidgets.QGraphicsItem.ItemSendsGeometryChanges)
+        self.start = None
+        self.end = None
+        self.state = NO_STATE
+        
     def mousePressEvent(self, event):
-        print "item press"
-        event.accept()
-    
+        print "item press", event.scenePos()
+        if QtGui.QVector2D(event.scenePos() - self.sceneBoundingRect().bottomRight()).length() < 10:
+            self.state = RESIZE
+            self.start = event.scenePos()
+        else:
+            self.state = MOVE
+        super(QGraphicsRectItem, self).mousePressEvent(event)
+
+    def mouseMoveEvent(self, event):
+        if self.state == RESIZE:
+            sp = event.scenePos()
+            d = sp - self.start
+            r = self.rect()
+            r.setBottomRight(sp)
+            self.setRect(r)
+            self.start = event.scenePos()
+            event.ignore()
+        else:
+            super(QGraphicsRectItem, self).mouseMoveEvent(event)
+        
+    def mouseReleaseEvent(self, event):
+        print "item release"
+        self.end = event.scenePos()
+        self.state = NO_STATE
+        super(QGraphicsRectItem, self).mouseReleaseEvent(event)
+        
 class QGraphicsView(QtWidgets.QGraphicsView):
     def __init__(self, *args, **kwargs):
         super(QGraphicsView, self).__init__(*args, **kwargs)
         self.start = None
+
+class QGraphicsScene(QtWidgets.QGraphicsScene):
+    def __init__(self, *args, **kwargs):
+        super(QGraphicsScene, self).__init__(*args, **kwargs)
+        self.start = None
         
     def mousePressEvent(self, event):
-        print "view press"
-        self.start = event.pos()
-
-        super(QGraphicsView, self).mousePressEvent(event)
+        print "blah press", event.scenePos()
+        self.start = event.scenePos()
+        super(QGraphicsScene, self).mousePressEvent(event)
 
     def mouseReleaseEvent(self, event):
-        end = event.pos()
-        if self.start:
-            r = QGraphicsRectItem(QtCore.QRectF(self.start, end))
-            self.start = None
-            self.scene().addItem(r)
+        print "blah release", event.scenePos()
+        if self.mouseGrabberItem():
+            print "there is a mouse grabber, not adding!"
         else:
-            print "missing start"
-        
-        super(QGraphicsView, self).mouseReleaseEvent(event)
+            end = event.scenePos()
+            print self.start, end
+            r = QtCore.QRectF(self.start, end)
+            print r
+            self.addItem(QGraphicsRectItem(r))
+            self.start = None
+        super(QGraphicsScene, self).mouseReleaseEvent(event)
         
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -42,7 +78,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.view = QGraphicsView()
         self.view.setDragMode(QtWidgets.QGraphicsView.RubberBandDrag)
-        self.scene = QtWidgets.QGraphicsScene()
+        self.scene = QGraphicsScene()
         self.view.setScene(self.scene)
 
         self.control_widget = QtWidgets.QWidget(self)
