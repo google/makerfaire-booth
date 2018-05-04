94a8219fd315c9e3d51b2dbf9a35abf2492d1e37
dek <dakoner@gmail.com>
2018-05-04 09:10:07 -0700

    make label_burger.py python3

--- 2018/burger/machine/label_burger.py
+++ 2018/burger/machine/label_burger.py
@@ -2,7 +2,7 @@ from constants import MAX_BURGER_HEIGHT
 from burger_elements import BurgerElement
 def label_burger(burger, debug=False):
   if len(burger) != MAX_BURGER_HEIGHT:
-    if debug: print "Burger is wrong size"
+    if debug: print("Burger is wrong size")
     return False
   topbun_pos = None
   for i in range(len(burger)):
@@ -13,32 +13,32 @@ def label_burger(burger, debug=False):
     if layer == BurgerElement.topbun.value:
       topbun_pos = i
       break
-    if debug: print "prefix elements must be empty or topbun"
+    if debug: print("prefix elements must be empty or topbun")
     return False
   if topbun_pos is None:
-    if debug: print "Burger does not have topbun"
+    if debug: print("Burger does not have topbun")
     return False
   if burger[-1] != BurgerElement.bottombun.value:
-    if debug: print "Burger does not have bottombun bottom"
+    if debug: print("Burger does not have bottombun bottom")
     return False
   for i in range(topbun_pos+1, len(burger)-1):
     if burger[i] == BurgerElement.topbun.value or burger[i] == BurgerElement.bottombun.value:
-      if debug: print "Cannot have internal buns"
+      if debug: print("Cannot have internal buns")
       return False
   for i in range(topbun_pos+1, len(burger)):
     if burger[i] == BurgerElement.empty.value:
-      if debug: print "Cannot have internal or terminal empty"
+      if debug: print("Cannot have internal or terminal empty")
       return False
   for i in range(len(burger)-1):
     first, second = burger[i], burger[i+1]
     if first != BurgerElement.empty.value and second != BurgerElement.empty.value and first == second:
-      if debug: print "Cannot have identical sequential items"
+      if debug: print("Cannot have identical sequential items")
       return False
   for i in range(1, len(burger)-1):
     if burger[i] == BurgerElement.patty.value:
       break
   else:
-    if debug: print "Must have at least one patty"
+    if debug: print("Must have at least one patty")
     return False
   last_cheese_index = None
   for i in range(1, len(burger)-1):
@@ -49,6 +49,6 @@ def label_burger(burger, debug=False):
       if burger[i] == BurgerElement.patty.value:
         break
     else:
-      if debug: print "Must have at patty under last cheese."
+      if debug: print("Must have at patty under last cheese.")
       return False
   return True
