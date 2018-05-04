19024ebf56672bebe5404e78c6fe91022b6c8327
David Konerding <dakoner@gmail.com>
2018-05-04 11:03:57 -0700

    remove predict server from top-level server

---
+++ 2018/burger/experimental/dek/predict_server/server.py
@@ -0,0 +1,29 @@
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import predict
+
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write('ok')
+
+urls = [
+    (r"/", IndexHandler),
+    (r"/predict", predict.PredictHandler),
+]
+
+settings = dict({
+        "debug": False,
+    })
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(urls, **settings)
+    http_server = tornado.httpserver.HTTPServer(app)
+
+    http_server.bind(8888)
+    http_server.start(10)
+    tornado.ioloop.IOLoop.current().start()
