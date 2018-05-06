91d4216b85c6697e245bf510cc2c9340f92be93e
David Konerding <dakoner@gmail.com>
2018-05-06 10:09:28 -0700

    updates

--- 2018/burger/experimental/dek/test/model.py
+++ 2018/burger/experimental/dek/test/model.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 column_names = ['layer0', 'layer1', 'layer2', 'layer3', 'layer4', 'layer5']
 import sqlite3
 
@@ -34,25 +35,35 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
     
     clf = MLPClassifier(solver='adam',  activation='relu',
-                        hidden_layer_sizes=(32,32),
+                        hidden_layer_sizes=64,
                         verbose=False,
                         max_iter=10000,
                         tol=1e-9,
                         random_state=1)
-    sample = X_train.sample(1).join(y_train)
     classes = numpy.unique(y)
+    i = 0
     while True:
+        burgers = X_train[y_train == 1]
+        notburgers = X_train[y_train == 0]
+        # Pull 32 samples from training data,
+        # where half the samples come from each class
+        sample = burgers.sample(16).join(y_train)
+        sample = sample.append(notburgers.sample(16).join(y_train))
         sample_X_train = sample.drop(['output'], axis=1)
         sample_y_train = sample['output']
         sample_X_train_categoricals = sample_X_train[column_names]
         tX_sample_train_categoricals = enc.fit_transform(sample_X_train_categoricals)
         clf.partial_fit(tX_sample_train_categoricals, sample_y_train.as_matrix().astype(int), classes=classes)
 
-        X_test_categoricals = X_test[column_names]
-        tX_test_categoricals = enc.fit_transform(X_test_categoricals)
-        prediction = clf.predict(tX_test_categoricals)
-        print_eval(y_test, prediction)
-        
+        if (i % 100) == 0:
+            print(i)
+            X_test_categoricals = X_test[column_names]
+            tX_test_categoricals = enc.fit_transform(X_test_categoricals)
+            prediction = clf.predict(tX_test_categoricals)
+            print_eval(y_test, prediction)
+            print(classification_report(y_test, prediction))
+        i += 1
+
         X_train_categoricals = X_train[column_names]
         tX_train_categoricals = enc.fit_transform(X_train_categoricals)
         probs = clf.predict_proba(tX_train_categoricals)
@@ -65,14 +76,12 @@ def main():
         tX_train_categoricals = enc.fit_transform(X_train_categoricals)
         prediction = clf.predict(tX_train_categoricals)
 
-        # Take worst mispredicted items and retrain
-        mispredicted = X_train_copy[y_train != prediction]
-        worst_burger = mispredicted.nlargest(1, "prob_burger", keep='first').drop(['prob_burger', 'prob_notburger'], axis=1).join(y_train)
-        worst_notburger = mispredicted.nsmallest(1, "prob_burger", keep='last').drop(['prob_burger', 'prob_notburger'], axis=1).join(y_train)
+        # # # Take worst mispredicted items and retrain
+        # mispredicted = X_train_copy[y_train != prediction]
+        # worst_burger = mispredicted.nlargest(1, "prob_burger", keep='first').drop(['prob_burger', 'prob_notburger'], axis=1).join(y_train)
+        # worst_notburger = mispredicted.nsmallest(1, "prob_burger", keep='last').drop(['prob_burger', 'prob_notburger'], axis=1).join(y_train)
 
-        ol = len(sample)
-        sample = sample.append(worst_burger)
-        sample = sample.append(worst_notburger)
-        print(ol,len(sample))
+        # sample = sample.append(worst_burger)
+        # sample = sample.append(worst_notburger)
 if __name__ == '__main__':
     main()
---
+++ 2018/burger/experimental/dek/test/simplemodel.py
@@ -0,0 +1,60 @@
+from __future__ import print_function
+import pickle
+import pandas
+import numpy
+import matplotlib.pyplot as plt
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import accuracy_score, confusion_matrix, roc_curve, roc_auc_score
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+column_names = ['layer0', 'layer1', 'layer2', 'layer3', 'layer4', 'layer5']
+import sqlite3
+
+def print_eval(y, prediction):
+    print(accuracy_score(y, prediction))
+    cf = confusion_matrix(y, prediction)
+
+    tp, fp, tn, fn = cf[1][1], cf[0][1], cf[0][0], cf[1][0]
+    
+    print("TP:", tp)
+    print("FP:", fp)
+    print("TN:", tn)
+    print("FN:", fn)
+    print("FPR:", fp/float(fp+tn))
+    print("FNR:", fn/float(fn+tp))
+
+def main():
+    enc = OneHotEncoder(n_values=[7,7,7,7,7,7])
+    conn = sqlite3.connect('server.db')
+    cursor = conn.cursor()
+    all_ = pandas.read_sql_query('SELECT layers.burger, labels.output, layers.layer0, layers.layer1, layers.layer2, layers.layer3, layers.layer4, layers.layer5 FROM layers,labels WHERE layers.burger = labels.burger', conn, index_col='burger')
+    
+    X = all_.drop(['output'], axis=1)
+    y = all_['output']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+
+               
+    clf = MLPClassifier(solver='adam',  activation='relu',
+                        verbose=False,
+                        max_iter=10000,
+                        tol=1e-9,
+                        random_state=1)
+    
+    X_train_categoricals = X_train[column_names]
+    tX_train_categoricals = enc.fit_transform(X_train_categoricals)
+    clf.fit(tX_train_categoricals, y_train.as_matrix().astype(int))
+
+    
+    X_test_categoricals = X_test[column_names]
+    tX_test_categoricals = enc.fit_transform(X_test_categoricals)
+    prediction = clf.predict(tX_test_categoricals)
+    
+    print(classification_report(y_test, prediction))
+    
+    print_eval(y_test, prediction)
+
+if __name__ == '__main__':
+    main()
