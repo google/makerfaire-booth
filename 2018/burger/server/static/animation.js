91e2378a81bc90fe90f0bd186a2e2d4a6ed7dd5f
David Konerding <dakoner@gmail.com>
2018-05-12 14:49:00 -0700

    Replace chute and conveyor lines with assets

--- 2018/burger/server/static/animation.js
+++ 2018/burger/server/static/animation.js
@@ -1,9 +1,10 @@
 const MAX_BURGERS = 6;
 const BURGER_LAYER_SPACING = 40;
 const BASE_INITIAL_Y = -300;
-const BASE_FINAL_Y = 2000;
+const BASE_FINAL_Y = 500;
 const BASE_ELEVATOR_FINAL_Y = 0;
-
+const BASE_CONVEYOR_Y = 700;
+const X_TARGET = 570;
 
 const layers_enum = Object.freeze({
     0: "empty",
@@ -25,49 +26,6 @@ const layer_names_enum = Object.freeze({
     "bottombun":6
 });
 
-function create_chute(center_x, height) {
-    var svgNS = 'http://www.w3.org/2000/svg';
-    var chute = document.createElementNS(svgNS, "svg");
-    chute.setAttributeNS(null, 'id', 'chute');
-    chute.setAttributeNS(null, 'class', 'chute');
-    document.body.appendChild(chute);
-
-    var SIZE=Math.floor(128);
-    var chuteline1 = document.createElementNS(svgNS, 'line');
-    chuteline1.setAttributeNS(null, 'class', 'chuteline');
-    chuteline1.setAttributeNS(null, 'style', 'stroke:rgb(255,0,0);stroke-width:5');
-    chuteline1.setAttributeNS(null, 'x1', center_x - SIZE);
-    chuteline1.setAttributeNS(null, 'y1', 0);
-    chuteline1.setAttributeNS(null, 'x2', center_x - SIZE);
-    chuteline1.setAttributeNS(null, 'y2', height);
-    chute.appendChild(chuteline1);
-
-    var chuteline2 = document.createElementNS(svgNS, 'line');
-    chuteline2.setAttributeNS(null, 'class', 'chuteline');
-    chuteline2.setAttributeNS(null, 'style', 'stroke:rgb(255,0,0);stroke-width:5');
-    chuteline2.setAttributeNS(null, 'x1', center_x + SIZE);
-    chuteline2.setAttributeNS(null, 'y1', 0);
-    chuteline2.setAttributeNS(null, 'x2', center_x + SIZE);
-    chuteline2.setAttributeNS(null, 'y2', height);
-    chute.appendChild(chuteline2);
-}
-
-function create_conveyor(width, height) {
-    var svgNS = 'http://www.w3.org/2000/svg';
-    var conveyor = document.createElementNS(svgNS, "svg");
-    conveyor.setAttributeNS(null, 'id', 'conveyor');
-    conveyor.setAttributeNS(null, 'class', 'conveyor');
-    document.body.appendChild(conveyor);
-    var conveyorLine = document.createElementNS(svgNS, 'line');
-    conveyorLine.setAttributeNS(null, 'class', 'chuteline');
-    conveyorLine.setAttributeNS(null, 'style', 'stroke:rgb(255,0,0);stroke-width:5');
-    conveyorLine.setAttributeNS(null, 'x1', 0);
-    conveyorLine.setAttributeNS(null, 'y1', height);
-    conveyorLine.setAttributeNS(null, 'x2', width);
-    conveyorLine.setAttributeNS(null, 'y2', height);
-    conveyor.appendChild(conveyorLine);
-}
-
 function create_animation_burger_drop(element_name, data, wrapper) {
     var element = wrapper.children.namedItem(element_name);
     var body = document.getElementsByTagName("BODY")[0];
@@ -83,11 +41,9 @@ function create_animation_burger_drop(element_name, data, wrapper) {
 	delay: data.delay,
     }
 
-    var target = 256;
-
     var keyframes = [
-	{ transform: 'translateX(' + target + 'px) translateY(' + data.initialY + ')', opacity: 1},
-	{ transform: 'translateX(' + target + 'px) translateY(' + data.conveyorY + ')', opacity: 1},
+	{ transform: 'translateX(' + X_TARGET + 'px) translateY(' + data.initialY + ')', opacity: 1},
+	{ transform: 'translateX(' + X_TARGET + 'px) translateY(' + data.conveyorY + ')', opacity: 1},
     ];
     return new KeyframeEffect(element, keyframes, timings);
 }
@@ -96,9 +52,9 @@ function create_layer_height_offsets(spacing=BURGER_LAYER_SPACING) {
     var body = document.getElementsByTagName('body')[0];
     var width = body.getBoundingClientRect().width;
     var height = body.getBoundingClientRect().height;
-    var baseConveyorY = height-100-10;
+    var baseConveyorY = BASE_CONVEYOR_Y;
     var animation = [];
-    for (i = 0; i < 8; i++) {
+    for (i = 0; i < MAX_BURGERS + 2; i++) {
 	animation.push(["layer" + (MAX_BURGERS-i+1), {
 	    delay: i*100,
 	    initialY: (BASE_INITIAL_Y - i * spacing) + 'px',
@@ -129,6 +85,8 @@ function create_burger(layers) {
     for (i = 0; i < layers.length; i++) {
     	var svg = document.createElementNS(svgNS, "svg");
     	svg.setAttributeNS(null, 'viewBox', '0 0 71.25 40');
+    	// svg.setAttributeNS(null, 'height', '35px');
+    	// svg.setAttributeNS(null, 'width', '20px');
     	svg.setAttributeNS(null, 'id', 'layer' + (i+1).toString());
     	svg.setAttributeNS(null, 'class', layers[i]);
 	if (layers[i] != 'empty') {
@@ -150,6 +108,7 @@ function layer_idx_to_layers(layers_idx) {
     return layers;
 
 }
+
 function burger_drop_animation(layers) {
     console.log("Create burger", layers);
     var wrapper = create_burger(layers);
@@ -202,10 +161,9 @@ function create_animation_trash_burger(element_name, data, wrapper) {
 	direction: "normal",
 	fill: "forwards",
     }
-    var target = 256;
-    var keyframes = [
-	{ transform: 'translateX(' + target + 'px) translateY(' + data.conveyorY + ')', opacity: 1},
-	{ transform: 'translateX(' + target + 'px) translateY(' + data.finalY + ')', opacity: 1},
+        var keyframes = [
+	{ transform: 'translateX(' + X_TARGET + 'px) translateY(' + data.conveyorY + ')', opacity: 1},
+	{ transform: 'translateX(' + X_TARGET + 'px) translateY(' + data.finalY + ')', opacity: 1},
     ];
     return new KeyframeEffect(element, keyframes, timings);
 }
@@ -223,14 +181,12 @@ function create_animation_convey_burger_to_middle(element_name, data, wrapper) {
 	direction: "normal",
 	fill: "forwards",
     }
-    var target = 256;
-    var keyframes = [
-	{ transform: 'translateX(' + target + 'px) translateY(' + data.conveyorY + ')', opacity: 1},
+        var keyframes = [
+	{ transform: 'translateX(' + X_TARGET + 'px) translateY(' + data.conveyorY + ')', opacity: 1},
 	{ transform: 'translateX(' + width/2 + 'px) translateY(' + data.conveyorY + ')', opacity: 1},
     ];
     return new KeyframeEffect(element, keyframes, timings);
 }
-
 
 function create_animation_smoosh_burger(element_name, data1, data2, wrapper) {
     var element = wrapper.children.namedItem(element_name);
@@ -247,11 +203,10 @@ function create_animation_smoosh_burger(element_name, data1, data2, wrapper) {
 	delay: data1.delay,
     }
 
-    var target = 256;
-
+    
     var keyframes = [
-	{ transform: 'translateX(' + target + 'px) translateY(' + data1.conveyorY + ')', opacity: 1},
-	{ transform: 'translateX(' + target + 'px) translateY(' + data2.conveyorY + ')', opacity: 1},
+	{ transform: 'translateX(' + X_TARGET + 'px) translateY(' + data1.conveyorY + ')', opacity: 1},
+	{ transform: 'translateX(' + X_TARGET + 'px) translateY(' + data2.conveyorY + ')', opacity: 1},
     ];
     return new KeyframeEffect(element, keyframes, timings);
 }
@@ -424,8 +379,6 @@ const body = document.getElementsByTagName('body')[0];
 body.onload = function() {
     var width = body.getBoundingClientRect().width;
     var height = body.getBoundingClientRect().height;
-    create_conveyor(width, height-100);
-    create_chute(256, height-100);
     requestStatus();
     start_burger_drop_animation();
 };
