3b40bbed481c6353af09bdf2fdda7ef2ce7ae3ec
David Konerding <dek@google.com>
2018-05-15 10:07:39 -0700

    Add r and p buttons.

--- 2018/burger/kiosk/BurgerButtons/BurgerButtons.ino
+++ 2018/burger/kiosk/BurgerButtons/BurgerButtons.ino
@@ -1,6 +1,9 @@
 #include <Keyboard.h>
 int buttonZPin = 2;  // Set a button to any pin
 int buttonXPin = 3;  // Set a button to any pin
+int buttonRPin = 4;  // Set a button to any pin
+int buttonPPin = 5;  // Set a button to any pin
+
 
 void setup()
 {
@@ -8,6 +11,10 @@ void setup()
   digitalWrite(buttonZPin, HIGH);  // Pull the button high
   pinMode(buttonXPin, INPUT);  // Set the button as an input
   digitalWrite(buttonXPin, HIGH);  // Pull the button high
+  pinMode(buttonRPin, INPUT);  // Set the button as an input
+  digitalWrite(buttonRPin, HIGH);  // Pull the button high
+  pinMode(buttonPPin, INPUT);  // Set the button as an input
+  digitalWrite(buttonPPin, HIGH);  // Pull the button high
 }
 
 void loop()
@@ -21,6 +28,16 @@ void loop()
   {
     Keyboard.write('x');  // send a 'x' to the computer via Keyboard HID
     delay(1000);  // delay so there aren't a kajillion x's
+  }
+  if (digitalRead(buttonRPin) == 0)  // if the button goes low
+  {
+    Keyboard.write('r');  // send a 'r' to the computer via Keyboard HID
+    delay(1000);  // delay so there aren't a kajillion r's
+  }
+  if (digitalRead(buttonPPin) == 0)  // if the button goes low
+  {
+    Keyboard.write('p');  // send a 'p' to the computer via Keyboard HID
+    delay(1000);  // delay so there aren't a kajillion p's
   }
 }
 
