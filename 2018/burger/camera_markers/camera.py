a0ce0254dd5c91dcfbfe72774eecad135715e6ac
dek <dakoner@gmail.com>
2018-05-17 14:44:56 -0700

    qgraphicsview-based UI.

--- 2018/burger/camera_markers/camera.py
+++ 2018/burger/camera_markers/camera.py
@@ -18,6 +18,12 @@ filename="z:/cut.mkv"
 
 labels = dict([(member.value, member.name) for member in BurgerElement.__members__.values()])
 
+WIDTH=3840
+HEIGHT=2160
+
+CAMERA_WIDTH=1280
+CAMERA_HEIGHT=720
+
 WARPED_HEIGHT=720
 ratio = 0.4
 WARPED_WIDTH = int(round(WARPED_HEIGHT * ratio))
@@ -33,36 +39,41 @@ def intersection(a,b):
 def boxCenter(box):
     return [box[0] + (box[2]-box[0])/2, box[1] + (box[3]-box[1])/2]
 
-class MainWindow(QtWidgets.QMainWindow):
+class MainWindow(QtWidgets.QGraphicsView):
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.central_widget = QtWidgets.QWidget(self)
-        self.central_layout = QtWidgets.QHBoxLayout()
-        self.central_widget.setLayout(self.central_layout)
-        self.setCentralWidget(self.central_widget)
-        self.image_widget = QtWidgets.QLabel(self)
-        self.image_widget.setFixedSize(1280, 720)
-        self.central_layout.addWidget(self.image_widget)
-        self.image2_widget = QtWidgets.QLabel(self)
-        self.image2_widget.setFixedSize(WARPED_WIDTH, WARPED_HEIGHT)
-        self.central_layout.addWidget(self.image2_widget)
-        self.image3_widget = QtWidgets.QLabel(self)
-        self.image3_widget.setFixedSize(1280, 720)
-        self.central_layout.addWidget(self.image3_widget)
-        self.image3_text = QtWidgets.QLabel(self)
-        self.central_layout.addWidget(self.image3_text)
-        
+        self.scene = QtWidgets.QGraphicsScene(self)
+        self.scene.setBackgroundBrush(QtGui.QBrush(QtGui.QColor(131, 213, 247)))
+        self.scene.setSceneRect(0, 0, WIDTH, HEIGHT)
+
+        self.machine = QtSvg.QGraphicsSvgItem('../static/assets/burger_chute.svg')
+        self.scene.addItem(self.machine)
+        self.machine.setScale(2)
+        self.machine.setPos(1000,0)
+ 
+        self.setFixedSize(WIDTH,HEIGHT)
+        self.setScene(self.scene)
+        self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+
         self.cam = cv2.VideoCapture(0)
-        self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-        self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-        # self.cam = cv2.VideoCapture(filename)
+        self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, CAMERA_WIDTH)
+        self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, CAMERA_HEIGHT)
+        # # self.cam = cv2.VideoCapture(filename)
         self.width = int(self.cam.get(cv2.CAP_PROP_FRAME_WIDTH))
         self.height = int(self.cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.cam_pixmap = None
         self.aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_1000)
         self.parameters =  cv2.aruco.DetectorParameters_create()
         rms, self.mtx, self.dist, rvecs, tvecs = pickle.load(open("calib.pkl","rb"))
         self.objdet = ObjectDetector()
-
+        self.burger_pixmap = None
+        self.rectified_pixmap = None
+        font = QtGui.QFont('Arial', 120)
+        self.label_text = self.scene.addText("NO", font)
+        self.label_text.setDefaultTextColor(QtCore.Qt.red)
+        self.label_text.setPos(1600,1150)
+        
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.camera)
         self.timer.start(0) 
@@ -70,10 +81,27 @@ class MainWindow(QtWidgets.QMainWindow):
         self.counter = 0
         self.burger_classifier = BurgerClassifier()
 
+        font = QtGui.QFont('Arial', 120)
+        self.explanation_text = self.scene.addText("test", font)
+        self.explanation_text.setHtml("This is <bold>html</bold> text.")
+        self.explanation_text.setPos(100, 100)
+        
     def imageTo(self, image):
         pixmap = QtGui.QPixmap.fromImage(image)
-        self.image_widget.setPixmap(pixmap)
+        if self.cam_pixmap is None:
+            self.cam_pixmap = self.scene.addPixmap(pixmap)
+            self.cam_pixmap.setPos(QtCore.QPointF(50, 2160-720-50))
+        else:
+            self.cam_pixmap.setPixmap(pixmap)
 
+    def imageTo2Clear(self):
+        if self.rectified_pixmap is not None:
+            self.scene.removeItem(self.rectified_pixmap)
+            self.rectified_pixmap = None
+        if self.burger_pixmap is not None:
+            self.scene.removeItem(self.burger_pixmap)
+            self.burger_pixmap = None
+            
     def imageTo2(self, image2, boxes):
         pixmap2 = QtGui.QPixmap.fromImage(image2)
         boxes = self.analyzeBoxes(boxes)
@@ -87,7 +115,11 @@ class MainWindow(QtWidgets.QMainWindow):
             p.drawRect(QtCore.QRect(QtCore.QPoint(x1, y1), QtCore.QSize(w, h)))
             p.drawText(x1, y1, "%s: %5.2f" % (labels[class_], score))
         p.end()
-        self.image2_widget.setPixmap(pixmap2)
+        if self.rectified_pixmap == None:
+            self.rectified_pixmap = self.scene.addPixmap(pixmap2)
+            self.rectified_pixmap.setPos(QtCore.QPointF(3840 - pixmap2.width(), 2160-pixmap2.height()))
+        else:
+            self.rectified_pixmap.setPixmap(pixmap2)
 
     def analyzeBoxes(self, boxes):
         all_intersections = []
@@ -153,15 +185,25 @@ class MainWindow(QtWidgets.QMainWindow):
         bits = image_128.constBits().asstring(128*128*3)
         img = np.fromstring(bits, dtype=np.uint8).reshape(128, 128, 3)
         pixmap3 = QtGui.QPixmap.fromImage(image)
-        self.image3_widget.setPixmap(pixmap3)
+        if self.burger_pixmap is None:
+            self.burger_pixmap = self.scene.addPixmap(pixmap3)
+            self.burger_pixmap.setPos(QtCore.QPointF(1370, 190))
+        else:
+            self.burger_pixmap.setPixmap(pixmap3)
 
         result = self.burger_classifier.classify(classes_)[0]
         if result[1] > 0.5:
             label = 'burger'
+            self.label_text.setPlainText("YES")
+            self.label_text.setDefaultTextColor(QtCore.Qt.green)
         else:
             label = 'notburger'
-        self.image3_text.setText("P(burger) = %5.2f P(notburger) = %5.2f (%s" % (result[1], result[0], label))
+            self.label_text.setPlainText("NO")
+            self.label_text.setDefaultTextColor(QtCore.Qt.red)
+        # self.image3_text.setText("P(burger) = %5.2f P(notburger) = %5.2f (%s" % (result[1], result[0], label))
+        self.label_text
         # label = label_burger(classes_)
+        
         return ordered_boxes_with_gaps
         
     def camera(self):
@@ -192,6 +234,8 @@ class MainWindow(QtWidgets.QMainWindow):
             if ids is None:
                 image = QtGui.QImage(img2.data, w3, h3, w3*3, QtGui.QImage.Format_RGB888).rgbSwapped()
                 self.imageTo(image)
+                self.imageTo2Clear()
+                return
             else:
                 short_ids = [id_[0] for id_ in ids]
                 for i, corner in enumerate(corners):
@@ -199,6 +243,7 @@ class MainWindow(QtWidgets.QMainWindow):
                 if len(d.keys()) != 4:
                     image = QtGui.QImage(img2.data, w3, h3, w3*3, QtGui.QImage.Format_RGB888).rgbSwapped()
                     self.imageTo(image)
+                    self.imageTo2Clear()
                     return
                 ul = d[0][0][0]
                 ur = d[1][0][0]
@@ -249,5 +294,6 @@ if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     app = QApplication(sys.argv)
     widget = MainWindow()
-    widget.show()
+    # widget.show()
+    widget.showFullScreen()
     app.exec_()
