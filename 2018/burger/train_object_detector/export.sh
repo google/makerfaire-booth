274cfa35bb15c766a63c76543c626f85fcbf7fbc
David Konerding <dakoner@gmail.com>
2018-05-17 02:26:16 +0000

    export step 10k.

--- 2018/burger/train_object_detector/export.sh
+++ 2018/burger/train_object_detector/export.sh
@@ -3,6 +3,6 @@ source conf.sh
 python $MODELS_RESEARCH_DIR/object_detection/export_inference_graph.py \
        --input_type=image_tensor \
        --pipeline_config_path=models/model/faster_rcnn_resnet101_burgers.config \
-       --trained_checkpoint_prefix=models/train/model.ckpt-1607 \
+       --trained_checkpoint_prefix=models/train/model.ckpt-10000 \
        --output_directory=output_inference_graph
        # --pipeline_config_path=models/model/ssd_mobilenet_v1_burgers.config \
