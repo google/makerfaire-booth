b58ffabbd9b11d0cb8bfe083ea3c4adcadf4ce2e
dek <dakoner@gmail.com>
2018-05-30 15:01:11 -0700

    add argo

--- 2018/burger/experimental/dek/objectdetection-trainer/create_kubeflow.sh
+++ 2018/burger/experimental/dek/objectdetection-trainer/create_kubeflow.sh
@@ -5,48 +5,51 @@
 export GITHUB_TOKEN=$(cat ~/github_token.txt)
 export PATH=~/ks_0.10.2_linux_amd64:$PATH
 
-#gcloud beta container --project seventh-oven-198801 clusters create cluster-1 --zone us-east1-c --username admin --cluster-version 1.10.2-gke.1 --machine-type n1-standard-8 --accelerator type=nvidia-tesla-p100,count=1 --image-type COS --disk-type pd-standard --disk-size 100 --scopes https://www.googleapis.com/auth/cloud-platform --num-nodes 3 --enable-cloud-logging --enable-cloud-monitoring --network default --subnetwork default --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autorepair
-
-#kubectl apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/container-engine-accelerators/stable/nvidia-driver-installer/cos/daemonset-preloaded.yaml
 
 set -e
 
+# gcloud beta container --project "ftc-research" clusters create "cluster-1" --zone "us-west1-a" --username "admin" --cluster-version "1.10.2-gke.3" --machine-type "custom-10-38400" --accelerator "type=nvidia-tesla-v100,count=1" --image-type "COS" --disk-type "pd-standard" --disk-size "100" --scopes "https://www.googleapis.com/auth/cloud-platform" --num-nodes "3" --enable-cloud-logging --enable-cloud-monitoring --network "default" --subnetwork "default" --enable-autoscaling --min-nodes "3" --max-nodes "9" --addons HorizontalPodAutoscaling,HttpLoadBalancing,KubernetesDashboard --enable-autoupgrade --enable-autorepair
+
+# kubectl apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/container-engine-accelerators/stable/nvidia-driver-installer/cos/daemonset-preloaded.yaml
 
 KUBEFLOW_VERSION=v0.1.2
 KF_GKE_ENV=gke
 KF_NAMESPACE=kubeflow
 
-# kubectl create namespace ${KF_NAMESPACE}
-# kubectl create clusterrolebinding default-admin --clusterrole=cluster-admin --user=dakoner@gmail.com
+kubectl create namespace ${KF_NAMESPACE}
+kubectl create clusterrolebinding default-admin --clusterrole=cluster-admin --user=dakoner@gmail.com
 
-# rm -rf my-kubeflow
-# ks init my-kubeflow
+rm -rf my-kubeflow
+ks init my-kubeflow
 
 cd my-kubeflow
-# ks registry add kubeflow github.com/kubeflow/kubeflow/tree/${KUBEFLOW_VERSION}/kubeflow
-# ks pkg install kubeflow/core@${KUBEFLOW_VERSION}
-# ks pkg install kubeflow/tf-serving@${KUBEFLOW_VERSION}
-# ks pkg install kubeflow/tf-job@${KUBEFLOW_VERSION}
+ks registry add kubeflow github.com/kubeflow/kubeflow/tree/${KUBEFLOW_VERSION}/kubeflow
+ks pkg install kubeflow/core@${KUBEFLOW_VERSION}
+ks pkg install kubeflow/tf-serving@${KUBEFLOW_VERSION}
+ks pkg install kubeflow/tf-job@${KUBEFLOW_VERSION}
+ks pkg install kubeflow/argo
 
-# ks generate core kubeflow-core --name=kubeflow-core
+ks generate core kubeflow-core --name=kubeflow-core
+ks generate argo kubeflow-argo --name=kubeflow-argo --imageTag=v2.1.0
 
-# ks param set kubeflow-core reportUsage true
-# ks param set kubeflow-core usageId $(uuidgen)
+ks param set kubeflow-core reportUsage true
+ks param set kubeflow-core usageId $(uuidgen)
 
 
-# ks env add $KF_GKE_ENV
-# ks param set kubeflow-core cloud gke --env=$KF_GKE_ENV
+ks env add $KF_GKE_ENV
+ks param set kubeflow-core cloud gke --env=$KF_GKE_ENV
+
 # ks param set kubeflow-core jupyterNotebookPVCMount /home/jovyan --env=$KF_GKE_ENV
 
-# ks env set ${KF_GKE_ENV} --namespace ${KF_NAMESPACE}
+ks env set ${KF_GKE_ENV} --namespace ${KF_NAMESPACE}
 
+ks apply ${KF_GKE_ENV} -c kubeflow-core
+ks apply ${KF_GKE_ENV} -c kubeflow-argo
 
-# ks apply ${KF_GKE_ENV} -c kubeflow-core
+# JOB_NAME=myob
 
-JOB_NAME=myob
-
-ks generate tf-job ${JOB_NAME} --name=${JOB_NAME}
-ks apply ${KF_GKE_ENV} -c ${JOB_NAME}
+# ks generate tf-job ${JOB_NAME} --name=${JOB_NAME}
+# ks apply ${KF_GKE_ENV} -c ${JOB_NAME}
 
 # WArning: opens port to internet
 # ks param set kubeflow-core jupyterHubServiceType LoadBalancer
