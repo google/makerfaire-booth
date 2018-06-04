50545dc439ca1e638bfa4209f0cd933fab472898
David Konerding <dek@google.com>
2018-06-04 11:54:26 -0700

    consolidate dockers

--- 2018/burger/experimental/dek/ftc-objectdetection-trainer/create_docker.sh
+++ 2018/burger/experimental/dek/ftc-objectdetection-trainer/create_docker.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -e
 
-GCR_PREFIX=gcr.io/ftc-research
+GCR_PREFIX=gcr.io/ftc-research/
 
 # TODO(dek): add :1.0 to version tags?
 
 cd containers
 
-#docker build . --build-arg TF_TAG=':latest' -t object-detection-base -f Dockerfile.base
-#docker build . --build-arg TF_TAG='@sha256:bfadad8f2c80424d8d6059d3b8cd6947bf23111dc786fc33db72b56b632a1f28' -t object-detection-base-gpu -f Dockerfile.base
-#docker build . --build-arg GPU_TAG='' -t ${GCR_PREFIX}/trainer -f Dockerfile.trainer
-#docker build . --build-arg GPU_TAG='-gpu' -t ${GCR_PREFIX}/trainer-gpu -f Dockerfile.trainer
-#docker build . -t ${GCR_PREFIX}/eval-gpu -f Dockerfile.eval-gpu 
+docker build . --build-arg TF_TAG=':latest' -t ${GCR_PREFIX}object-detection -f Dockerfile.base
+docker build . --build-arg TF_TAG='@sha256:bfadad8f2c80424d8d6059d3b8cd6947bf23111dc786fc33db72b56b632a1f28' -t ${GCR_PREFIX}object-detection-gpu -f Dockerfile.base
 
-docker push ${GCR_PREFIX}/trainer
-docker push ${GCR_PREFIX}/trainer-gpu
-docker push ${GCR_PREFIX}/eval-gpu
+docker push ${GCR_PREFIX}object-detection
+docker push ${GCR_PREFIX}object-detection-gpu
+# docker push ${GCR_PREFIX}/eval-gpu
