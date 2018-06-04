f7ca8cf9faef9a372340de167bdb839fecfa2d4a
dek <dakoner@gmail.com>
2018-06-03 19:54:02 -0700

    move Dockerfiles to submit.  Clean up scripts.

--- 2018/burger/experimental/dek/ftc-objectdetection-trainer/scripts/export.sh
+++ 2018/burger/experimental/dek/ftc-objectdetection-trainer/scripts/export.sh
@@ -5,5 +5,5 @@ export GS_PREFIX=gs://ftc-research-object-detector-train/
 python $MODELS_RESEARCH_DIR/object_detection/export_inference_graph.py \
        --input_type=image_tensor \
        --pipeline_config_path=${GS_PREFIX}models/model/faster_rcnn_resnet101.config \
-       --trained_checkpoint_prefix=${GS_PREFIX}models/train/model.ckpt-10817 \
+       --trained_checkpoint_prefix=${GS_PREFIX}models/train/model.ckpt-47889 \
        --output_directory=output_inference_graph
