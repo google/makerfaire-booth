0c344cf38f23ba7c2aa064adf1dce97deb0fbff1
David Konerding <dek@google.com>
2018-06-05 12:13:32 -0700

    add ssdlite_mobilenet_v2_coco

--- 2018/burger/experimental/dek/ftc-objectdetection-trainer/scripts/train_local.sh
+++ 2018/burger/experimental/dek/ftc-objectdetection-trainer/scripts/train_local.sh
@@ -3,5 +3,5 @@ MODELS_RESEARCH_DIR=/opt/tensorflow_models/research
 export PYTHONPATH=$MODELS_RESEARCH_DIR:$MODELS_RESEARCH_DIR/slim
 export GS_PREFIX=gs://ftc-research-object-detector-train/
 python /opt/tensorflow_models/research/object_detection/train.py \
-       --pipeline_config_path=${GS_PREFIX}models/model/faster_rcnn_resnet101.config \
+       --pipeline_config_path=${GS_PREFIX}models/model/ssdlite_mobilenet_v2_coco.config \
        --train_dir=${GS_PREFIX}models/train
