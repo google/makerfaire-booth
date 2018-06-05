b945824ff780769097ff44e28d674ce39c8b960e
David Konerding <dek@google.com>
2018-06-05 10:15:38 -0700

    move pod training to use command and args

--- 2018/burger/experimental/dek/ftc-objectdetection-trainer/scripts/eval_local.sh
+++ 2018/burger/experimental/dek/ftc-objectdetection-trainer/scripts/eval_local.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 MODELS_RESEARCH_DIR=/opt/tensorflow_models/research
 export PYTHONPATH=$MODELS_RESEARCH_DIR:$MODELS_RESEARCH_DIR/slim
 export GS_PREFIX=gs://ftc-research-object-detector-train/
-python $MODELS_RESEARCH_DIR/object_detection/train.py \
+python $MODELS_RESEARCH_DIR/object_detection/eval.py \
        --pipeline_config_path=${GS_PREFIX}models/model/faster_rcnn_resnet101.config \
-       --train_dir=${GS_PREFIX}models/train
+       --checkpoint_dir=${GS_PREFIX}models/train \
+       --eval_dir=${GS_PREFIX}models/eval
--- 2018/burger/experimental/dek/ftc-objectdetection-trainer/scripts/train_local.sh
+++ 2018/burger/experimental/dek/ftc-objectdetection-trainer/scripts/train_local.sh
@@ -2,7 +2,6 @@
 MODELS_RESEARCH_DIR=/opt/tensorflow_models/research
 export PYTHONPATH=$MODELS_RESEARCH_DIR:$MODELS_RESEARCH_DIR/slim
 export GS_PREFIX=gs://ftc-research-object-detector-train/
-python $MODELS_RESEARCH_DIR/object_detection/eval.py \
+python /opt/tensorflow_models/research/object_detection/train.py \
        --pipeline_config_path=${GS_PREFIX}models/model/faster_rcnn_resnet101.config \
-       --checkpoint_dir=${GS_PREFIX}models/train \
-       --eval_dir=${GS_PREFIX}models/eval
+       --train_dir=${GS_PREFIX}models/train
