ccbece9e5ddcf0710628382c91979128a4e97668
David Konerding <dek@google.com>
2018-06-06 13:22:04 -0700

    make layers read proto.

--- 2018/burger/experimental/dek/ftc-objectdetection-trainer/layers.py
+++ 2018/burger/experimental/dek/ftc-objectdetection-trainer/layers.py
@@ -1,1 +1,8 @@
-layers = ['white_whiffle', 'green_whiffle', 'yellow_cube']
+from google.protobuf import text_format
+from object_detection.protos import string_int_label_map_pb2
+
+LABEL_MAP = "data/label_map.pb.txt"
+f = open(LABEL_MAP).read()
+m = text_format.Parse(f, string_int_label_map_pb2.StringIntLabelMap())
+layers = [item.name for item in m.item]
+layer_dict = dict([(item.id, item.name) for item in m.item])
