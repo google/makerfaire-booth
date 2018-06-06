310147d95d4f23bf2b535d77bcc8e1ab4532eb5a
dek <dakoner@gmail.com>
2018-06-05 19:31:18 -0700

    eval ssdlist_mobilenet_v2_coo

--- 2018/burger/experimental/dek/ftc-objectdetection-trainer/scripts/eval_local.sh
+++ 2018/burger/experimental/dek/ftc-objectdetection-trainer/scripts/eval_local.sh
@@ -3,6 +3,6 @@ MODELS_RESEARCH_DIR=/opt/tensorflow_models/research
 export PYTHONPATH=$MODELS_RESEARCH_DIR:$MODELS_RESEARCH_DIR/slim
 export GS_PREFIX=gs://ftc-research-object-detector-train/
 python $MODELS_RESEARCH_DIR/object_detection/eval.py \
-       --pipeline_config_path=${GS_PREFIX}models/model/faster_rcnn_resnet101.config \
-       --checkpoint_dir=${GS_PREFIX}models/train \
+       --pipeline_config_path=${GS_PREFIX}models/model/ssdlite_mobilenet_v2_coco.config \
+       --checkpoint_dir=${GS_PREFIX}models/train_ssdlite_mobilenet_v2_coco.config \
        --eval_dir=${GS_PREFIX}models/eval
